a851a1d8bcc25f14afc325b1688d571ae7601e5d
kcurry <kcurry@localhost>
2018-02-21 17:19:09 -0500

    FLR-99 As a developer, I want to address security scan findings - Updated packageDeployment script, and added key and cert files to config directory.

--- misp-trans-client-rest-service/packageDeployment.sh
+++ misp-trans-client-rest-service/packageDeployment.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-rm -r ./deploy
+mtcdatetime=`date +%Y%m%d%H%M%S`
+echo $mtcdatetime
+echo This script will preserve the contents of /opt/mtc in the following TAR file: /tmp/${mtcdatetime}_opt_mtc.tar
+tar cvf /tmp/${mtcdatetime}_opt_mtc.tar /opt/mtc
+sudo mkdir -p /opt/mtc/out
+sudo chmod 775 /opt/mtc/out
+sudo mkdir -p /opt/mtc/config
+sudo chmod 775 /opt/mtc/config
+sudo cp ./config/* /opt/mtc/config
+sudo chmod 775 /opt/mtc/config/*
+/usr/bin/sudo chown -R flaredev:flaredev /opt/mtc
+sudo rm -rf ./deploy
 mvn clean package
 mkdir -p ./deploy/config
 cp ./target/*.jar ./deploy
